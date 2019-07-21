cc3debea7d8f3296cdf24083f9c40ca167ece65c
Chris Sells <csells@sellsbrothers.com>
2019-07-20 18:34:18 -0700

    added selection region

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,14 @@ class ImageCropper extends StatefulWidget {
 }
 
 class _ImageCropperState extends State<ImageCropper> {
+  static Color _kSelectionRectangleBackground = Color(0x15000000);
+  static Color _kSelectionRectangleBorder = Color(0x80000000);
   Color backgroundColor;
+  Rect region;
+  Rect dragRegion;
+  Offset startDrag;
+  Offset currentDrag;
+  final GlobalKey imageKey = GlobalKey();
 
   @override
   void initState() {
@@ -57,13 +64,80 @@ class _ImageCropperState extends State<ImageCropper> {
     setState(() => backgroundColor = paletteGenerator.dominantColor.color);
   }
 
+  // Called when the user starts to drag
+  void _onPanDown(DragDownDetails details) {
+    var box = imageKey.currentContext.findRenderObject() as RenderBox;
+    var localPosition = box.globalToLocal(details.globalPosition);
+    setState(() {
+      startDrag = localPosition;
+      currentDrag = startDrag;
+      dragRegion = Rect.fromPoints(startDrag, currentDrag);
+    });
+  }
+
+  // Called as the user drags
+  void _onPanUpdate(DragUpdateDetails details) {
+    setState(() {
+      currentDrag += details.delta;
+      dragRegion = Rect.fromPoints(startDrag, currentDrag);
+    });
+  }
+
+  // Called if the drag is canceled (e.g. by rotating the device or switching apps)
+  void _onPanCancel() {
+    setState(() {
+      dragRegion = null;
+      startDrag = null;
+    });
+  }
+
+  // Called when the drag ends
+  void _onPanEnd(DragEndDetails details) async {
+    Rect newRegion = (Offset.zero & imageKey.currentContext.size).intersect(dragRegion);
+    if (newRegion.size.width < 4 && newRegion.size.width < 4) {
+      newRegion = Offset.zero & imageKey.currentContext.size;
+    }
+    setState(() {
+      region = newRegion;
+      dragRegion = null;
+      startDrag = null;
+    });
+  }
+
   @override
-  Widget build(BuildContext context) => CustomPaint(
-        child: Center(
-          child: Container(
-            color: backgroundColor,
-            alignment: Alignment.center,
-            child: Image(image: widget.image),
+  Widget build(BuildContext context) => Container(
+        color: backgroundColor,
+        alignment: Alignment.center,
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          // GestureDetector is used to handle the selection rectangle
+          child: GestureDetector(
+            onPanDown: _onPanDown,
+            onPanUpdate: _onPanUpdate,
+            onPanCancel: _onPanCancel,
+            onPanEnd: _onPanEnd,
+            child: Stack(
+              children: [
+                Image(
+                  key: imageKey,
+                  image: widget.image,
+                ),
+                // This is the selection rectangle
+                Positioned.fromRect(
+                  rect: dragRegion ?? region ?? Rect.zero,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: _kSelectionRectangleBackground,
+                      border: Border.all(
+                        width: 1.0,
+                        color: _kSelectionRectangleBorder,
+                        style: BorderStyle.solid,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       );
